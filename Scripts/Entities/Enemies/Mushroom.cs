891388bc950e4080d83f79422a52bfaf43761c8f
Danpeo <danilvar4@gmail.com>
2024-05-05 02:05:33 +0400

    Revert "This commit is a mistake"
    This reverts commit bdf15db3e7c4b82720f98309c0e8fac99112a7b2.

--- Scripts/Entities/Enemies/Mushroom.cs
+++ Scripts/Entities/Enemies/Mushroom.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 using Godot;
 using Platformer.Scripts.Animations;
 using Platformer.Scripts.Constants;
@@ -67,7 +68,9 @@ public partial class Mushroom : CharacterBody2D, IEnemy, ISquashable
 
         var random = new Random();
         bool randomBoolValue = random.Next(0, 2) == 0;
+
         int direction = randomBoolValue ? -1 : 1;
+        
         Velocity = new Vector2(direction * 40f, -180f);
 
         PlayAnimation(MushroomAnim.Squash);
